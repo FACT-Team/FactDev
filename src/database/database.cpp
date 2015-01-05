5dd4144202b9f7043f7d6023e090c4c0f0ecbeba
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-05 08:46:41 +0100

    Add Database super class

---
+++ src/database/database.cpp
@@ -0,0 +1,219 @@
+#include "database/database.h"
+#include "log.h"
+
+#include <QStandardItem>
+#include <QDate>
+#include <QDebug>
+#include <QCoreApplication>
+#include <QFile>
+
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlRecord>
+
+// SINGLETON
+Database* Database::_instance = 0;
+bool Database::_bdInstance = 0;
+bool Database::isOpen = false;
+
+
+/**
+ * @brief Database::getInstance Return an instance of Database
+ * @return Instance of Database
+ */
+Database* Database::instance() throw(DbException*) {
+    if (_instance==0) {
+        _instance = new Database();
+    }
+
+    _instance->setDatabase(QSqlDatabase::database());
+    return _instance;
+}
+
+/**
+ * @brief Database::setDatabase Set database
+ * @param sql The new database
+ */
+void Database::setDatabase(QSqlDatabase sql) {
+    mDatabase = sql;
+}
+
+/**
+ * @brief Database::close Close database access
+ */
+void Database::close() {
+    mDatabase.close();
+}
+
+/**
+ * @brief Database::Database Database is a singleton
+ */
+Database::Database() throw(DbException*) {
+    if(!isOpen) {
+        mDatabase = QSqlDatabase::addDatabase("QSQLITE");
+        open();
+        isOpen = true;
+    }
+}
+
+/**
+ * @brief Database::~Database Suppression object, and close database access
+ */
+Database::~Database() {
+    mDatabase.close();
+    _instance = 0;
+    _bdInstance = 0;
+ /*   for(QList<Database*>::Iterator it = _instances.begin() ; it!=_instances.end() ; ++it) {
+        delete *it;
+    }
+
+    delete _instance;*/
+}
+
+
+/**
+ * @brief Database::open Open database
+ */
+void Database::open() {
+    mDatabase = QSqlDatabase::database();
+    bool creerStructure = false;
+
+    _settings = new QSettings("BilanMDT", "Antoine de Roquemaurel");
+    qDebug() << _settings->value("dbPath").toString();
+
+    if(_settings->value("dbPath").toString() == "") {
+        _settings->setValue("dbPath", QCoreApplication::applicationDirPath());
+    }
+    if(!QFile::exists(_settings->value("dbPath").toString()+"/database_0.5.db")) {
+        creerStructure = true;
+        _settings->setValue("version", 0);
+    }
+    Database::mDatabase.setDatabaseName(_settings->value("dbPath").toString()+"/database_0.5.db");
+
+    if(!Database::mDatabase.open()) {
+        mDatabase.close();
+        mDatabase = QSqlDatabase::database();
+        Database::mDatabase.setDatabaseName(_settings->value("dbPath").toString()+"/database_0.5.db");
+        if(!Database::mDatabase.open()) {
+            throw new DbException("Impossible d'ouvrir la base de données", "Database::Database", "Impossible d'ouvrir la base de données", 22.1);
+            exit(22);
+        }
+    }
+
+    QSqlQuery query;
+   // query.exec("PRAGMA foreign_keys = ON;");
+    query.exec("PRAGMA auto_vacuum=FULL");
+    query.exec("PRAGMA synchronous=OFF");
+    query.exec("PRAGMA journal_mode=MEMORY");
+    query.exec("PRAGMA default_cache_size=10000");
+    query.exec("PRAGMA locking_mode=EXCLUSIVE");
+    if(creerStructure) {
+        creerDatabase();
+
+        #ifndef QT_NO_DEBUG
+            jeuDEssai();
+        #endif
+    }
+}
+
+/**
+ * @brief Database::jeuDEssai Create
+ */
+inline void Database::jeuDEssai() {
+    executerFichier(QCoreApplication::applicationDirPath()+"/jeuEssai.sql");
+}
+
+/**
+ * @brief Database::viderDatabase Clear database
+ */
+inline void Database::viderDatabase() {
+    executerFichier(QCoreApplication::applicationDirPath()+"/viderDatabase.sql");
+}
+
+/**
+ * @brief Database::creerDatabase Create a new database
+ */
+inline void Database::   creerDatabase() {
+    executerFichier(QCoreApplication::applicationDirPath()+"/creation.sql");
+}
+
+/**
+ * @brief Database::executerFichier Exeute a specified file
+ * @param pNom File name
+ */
+void Database::executerFichier(QString pNom) {
+    QSqlQuery q;
+
+    Log::instance() << "Execution de " + pNom;
+
+    QFile file(pNom);
+    file.open(QIODevice::ReadOnly);
+
+    QStringList requetes = QString(file.readAll()).split(";", QString::SkipEmptyParts);
+    QStringListIterator i(requetes);
+    while(i.hasNext()) {
+        QString requete = i.next();
+        // Verifier que la requete n'est pas vide
+        if(requete.contains(QRegExp("[a-zA-Z]"))) {
+            q.prepare(requete);
+
+            if(!q.exec()) {
+                Log::instance(WARNING) << derniereErreur(q);
+            }
+        }
+    }
+    file.close();
+}
+
+/**
+ * @brief Database::openTransaction Open new transaction
+ */
+void Database::openTransaction()
+{
+    QSqlQuery q;
+
+    q.prepare("BEGIN TRANSACTION");
+    if(!q.exec()) {
+        Log::instance(WARNING) << "Erreur d'ouverture de la transaction";
+        Log::instance(WARNING) << derniereErreur(q);
+
+        throw std::exception();
+    }
+}
+
+/**
+ * @brief Database::closeTransaction Close current transaction
+ */
+void Database::closeTransaction()
+{
+    QSqlQuery q;
+
+    q.prepare("END TRANSACTION");
+
+    if(!q.exec()) {
+        Log::instance(WARNING) << "Erreur de fermeture de la transaction";
+        Log::instance(WARNING) << derniereErreur(q);
+    }
+
+}
+
+/**
+ * @brief Database::derniereErreur Return text of last error
+ * @param q Query
+ * @return error message
+ */
+inline QString Database::derniereErreur(const QSqlQuery& q) {
+    QString ret = "[ ERREUR  ] " + q.lastError().text()+"\n"
+                +"[ REQUETE ] " + q.lastQuery() +"\n";
+
+    return ret;
+}
+
+/**
+ * @brief Database::valeur Value of database field
+ * @param q Query
+ * @param champ Field
+ * @return  The value
+ */
+QVariant Database::value(const QSqlQuery& q, const QString& champ) {
+    return q.value(q.record().indexOf(champ));
+}
---
+++ src/database/database.h
@@ -0,0 +1,50 @@
+#ifndef Database_H
+#define Database_H
+
+#include <exception>
+#include <QtSql/QSql>
+#include <QtSql/QSqlQuery>
+#include <QStandardItemModel>
+#include <QSettings>
+
+
+#include "exceptions/dbexception.h"
+
+/**
+ * @brief The Database class Master class for all database access
+ */
+class Database {
+public:
+    static Database* instance() throw(DbException*);
+    QString  derniereErreur(const QSqlQuery& q);
+    void jeuDEssai();
+    void viderDatabase();
+    void executerFichier(QString pNom);
+    void openTransaction();
+    void closeTransaction();
+
+    void close();
+    void open();
+    ~Database();
+    void setDatabase(QSqlDatabase sql);
+
+protected:
+    Database()throw(DbException*);
+    static Database* _instance;
+    static bool _bdInstance;
+    QVariant value(const QSqlQuery& q, const QString& champ);
+    QSettings* _settings;
+
+    QSqlDatabase mDatabase;
+    QList<Database*> _instances;
+
+    static bool isOpen;
+
+
+private:
+    void creerDatabase();
+
+
+};
+
+#endif // Database_H
