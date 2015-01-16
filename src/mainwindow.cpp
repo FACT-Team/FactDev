31d5f93cc0d85bd8be9fd5f90d53c7290c25b89f
Cédric Rohaut <rohaut@icloud.com>
2015-01-16 11:37:50 +0100

    Change format of user information

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -84,12 +84,12 @@ void MainWindow::openCustomer()
     int id = getCurrentCustomerId();
     Customer *custom = new Customer(id);
     ui->lbAddress->setText(custom->getAddress());
-    ui->lbCompany->setText(custom->getCompany());
-    ui->lbEmail->setText(custom->getEmail());
+    ui->lbCompany->setText("<strong>" + custom->getCompany() + "</strong>");
+    ui->lbEmail->setText("<a href=\"mailto:" + custom->getEmail() + "\">" + custom->getEmail() + "</a>");
     ui->lbName->setText(custom->getLastnameReferent() + " " + custom->getFirstnameReferent());
-    ui->lbPhone->setText("Fixe :\t" + custom->getPhone());
-    ui->lbMobilePhone->setText("Mobile :\t" + custom->getMobilePhone());
-    ui->lbFax->setText("Fax :\t" +custom->getFax());
+    ui->lbPhone->setText("Fixe\t" + custom->getPhone());
+    ui->lbMobilePhone->setText("Mobile\t" + custom->getMobilePhone());
+    ui->lbFax->setText("Fax\t" +custom->getFax());
     ui->lbPostalCodeCityCountry->setText(custom->getPostalCode() + " " + custom->getCity() + ", " + custom->getCountry());
 
     //Popup::toImplement("MainWindow::openCustomer", this);
