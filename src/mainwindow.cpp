65bc0cde94259e7563a78841bd79252ad07eb5b8
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-16 10:18:16 +0100

    COrrection of problems… I'm sorry for that.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,7 @@
 #include "widgets/customercontextualmenu.h"
 #include "models/search.h"
 #include "widgets/popup.h"
+#include "dialogs/addprojectdialog.h"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -30,9 +31,14 @@ void MainWindow::addProject()
 
 }
 
+void MainWindow::changeCustomer()
+{
+
+    //Popup::toImplement("MainWindow::changeCustomer", this);
+}
+
 int MainWindow::getCurrentCustomerId() {
     QModelIndex idCell = ui->tblCustomers->model()->index(ui->tblCustomers->currentIndex().row(), 0);
-
     return ui->tblCustomers->model()->itemData(idCell).value(0).toInt();
 }
 
@@ -51,8 +57,17 @@ void MainWindow::addCustomer()
 
 void MainWindow::editCustomer()
 {
-    // TODO Implement me
-    Popup::toImplement("MainWindow::editCustomer", this);
+    DialogAddCustomer winAddCustomer(getCurrentCustomerId());
+    if(winAddCustomer.exec()) {
+        //qDebug() << "id : " << id << "\n";
+        updateTable();
+        updateTree();
+    }
+
+
+
+
+    //Popup::toImplement("MainWindow::editCustomer", this);
 }
 
 void MainWindow::removeCustomer()
@@ -87,7 +102,6 @@ void MainWindow::editUser()
     }
 }
 void MainWindow::search() {
-
     emit search(ui->leSearch->text());
 }
 
@@ -118,12 +132,19 @@ void MainWindow::search(QString text)
     } else {
         ui->leSearch->setStyleSheet(styleSearchBackground);
     }
+    s.setGroupFilter(ui->gpbxSearchFilter->isChecked());
+    s.setSearchInCompanies(ui->chkSearchCompany->isChecked());
+    s.setSearchInReferentLastname(ui->chkReferentName->isChecked());
+    s.setText(text);
+    updateTable(s.getFilter());
+    updateTree(s.getFilter());
 }
 
 void MainWindow::openContextualMenuTable(const QPoint point)
 {
     QMenu* menu = new CustomerContextualMenu(this);
 
+    emit changeCustomerTable();
     QPoint buffPoint = point;
     buffPoint.setX(point.x()+35);
     buffPoint.setY(point.y()+35);
@@ -133,26 +154,29 @@ void MainWindow::openContextualMenuTree(const QPoint point)
 {
     QMenu* menu = new CustomerContextualMenu(this);
 
+    emit changeCustomerTree();
     QPoint buffPoint = point;
     buffPoint.setX(point.x()+35);
     buffPoint.setY(point.y()+35);
     menu->exec(ui->trCustomers->mapToGlobal(buffPoint));
+
 }
 void MainWindow::updateTable(QString filter)
 {
     ui->tblCustomers->setModel(CustomerDatabase::instance()->getCustomersTable(filter));
     ui->tblCustomers->hideColumn(0);
     ui->tblCustomers->setColumnWidth(0, 100);
-    ui->tblCustomers->setColumnWidth(1, 100);
-    ui->tblCustomers->setColumnWidth(2, 150);
+    ui->tblCustomers->setColumnWidth(1, 150);
+    ui->tblCustomers->setColumnWidth(2, 100);
     ui->tblCustomers->setColumnWidth(3, 150);
-    ui->tblCustomers->setColumnWidth(4, 100);
+    ui->tblCustomers->setColumnWidth(4, 150);
     ui->tblCustomers->setColumnWidth(5, 200);
 }
 
 void MainWindow::updateTree(QString filter)
 {
     ui->trCustomers->setModel(CustomerDatabase::instance()->getCustomersTree(filter));
+    ui->trCustomers->header()->close();
 }
 
 void MainWindow::updateUserData()
@@ -167,5 +191,64 @@ void MainWindow::updateUserData()
     ui->lbPhone->setText(user.getPhone());
 
     user.commit();
+}
+
+void MainWindow::newProject()
+{
+    AddProjectDialog w;
+    if(w.exec()) {
+
+    }
+}
+
+void MainWindow::aboutQt()
+{
+    QMessageBox::aboutQt(this);
+}
+
+void MainWindow::aboutFact()
+{
+    QMessageBox::about(this, "About Fact", "Fact est une équipe de Développement créée "
+                                           "dans le cadre de projets pour l'Université Toulouse III - Paul Sabatier. <br />"
+                                      "Cette équipe est composée de : "
+                                      "<ul>"
+                                           "<li>Florent Berbie</li>"
+                                           "<li>Manantsoa Andriamihary Razanajatovo</li>"
+                                           "<li>Cédric Rohaut</li>"
+                                           "<li>Antoine de Roquemaurel</li>"
+                                      "</ul>");
+}
+
+void MainWindow::aboutFactDev()
+{
+    QMessageBox::about(this,"About FactDev","FactDev est un logiciel de Facture et Devis développé par l'équipe FACT "
+                                            "dans le cadre de l'UE Projet pour l'université Toulouse III - Paul Sabatier.");
+}
+
+void MainWindow::aboutIcons()
+{
+     QMessageBox::about(this,"About Icons","Le pack d'icons à été développé par Florent Berbie pour l'usage du logiciel FactDev");
+}
+
+void MainWindow::changeCustomerTree(QModelIndex index)
+{
+    ui->tblCustomers->selectRow(index.row()-1);
+    emit changeCustomer();
+}
+
+void MainWindow::changeCustomerTree()
+{
+    emit changeCustomerTree(ui->trCustomers->model()->index(ui->trCustomers->currentIndex().row(), 0));
+}
+
+void MainWindow::changeCustomerTable(QModelIndex index)
+{
+    //ui->trCustomers->set(index.row()+1);
+    emit changeCustomer();
+}
+
+void MainWindow::changeCustomerTable()
+{
+    emit changeCustomerTable(ui->tblCustomers->model()->index(ui->tblCustomers->currentIndex().row(), 0));
 }
 
