69b4e3dfca4797d9ea325d2de7ce8d39c3ad8d9f
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-01-22 18:38:15 +0100

    CustomerData display only no null values : #48

--- src/widgets/customerdatawidget.cpp
+++ src/widgets/customerdatawidget.cpp
@@ -20,32 +20,50 @@ void CustomerDataWidget::printUserData()
 {
     show();
     User *user = new User(1);
-    ui->lbCompany->setText(user->getCompany());
-    ui->lbName->setText(user->getLastname()+ " " + user->getFirstname());
-    ui->lbPhone->setText(user->getPhone());
-    ui->lbMobilePhone->setText(user->getMobilePhone());
-    ui->lbEmail->setText("<a href=\"mailto:" + user->getEmail() + "\">"
-                         + user->getEmail() + "</a>");
-    ui->lbAddress->setText(user->getAddress());
-    ui->lbPostalCodeCityCountry->setText(user->getPostalCode()
-                                         + " " + user->getCity());
     ui->lbFax->hide();
+    ui->lbTitleFax->hide();
+
+    setLabel(ui->lbCompany, user->getCompany());
+    setLabel(ui->lbName, user->getLastname()+ " " + user->getFirstname());
+    setLabel(ui->lbPhone, user->getPhone(), ui->lbTitlePhone);
+    setLabel(ui->lbMobilePhone, user->getMobilePhone(), ui->lbTitleMobile);
+    setLabel(ui->lbEmail, "<a href=\"mailto:" + user->getEmail() + "\">"
+             + user->getEmail() + "</a>");
+    setLabel(ui->lbAddress, user->getAddress());
+    setLabel(ui->lbPostalCodeCityCountry,user->getPostalCode()
+             + " " + user->getCity());
+
 }
 
 void CustomerDataWidget::printInformations(int id)
 {
     show();
     Customer *custom = new Customer(id);
-    ui->lbAddress->setText(custom->getAddress());
-    ui->lbCompany->setText(custom->getCompany());
-    ui->lbEmail->setText("<a href=\"mailto:" + custom->getEmail() + "\">"
-                         + custom->getEmail() + "</a>");
-    ui->lbName->setText(custom->getLastnameReferent() + " "
-                        + custom->getFirstnameReferent());
-    ui->lbPhone->setText(custom->getPhone());
-    ui->lbMobilePhone->setText(custom->getMobilePhone());
-    ui->lbFax->setText(custom->getFax());
-    ui->lbPostalCodeCityCountry->setText(custom->getPostalCode()
-                                         + " " + custom->getCity()
-                                         + ", " + custom->getCountry());
+    setLabel(ui->lbCompany, custom->getCompany());
+    setLabel(ui->lbName, custom->getLastnameReferent()+ " " + custom->getFirstnameReferent());
+    setLabel(ui->lbPhone, custom->getPhone(), ui->lbTitlePhone);
+    setLabel(ui->lbMobilePhone, custom->getMobilePhone(), ui->lbTitleMobile);
+    setLabel(ui->lbEmail, "<a href=\"mailto:" + custom->getEmail() + "\">"
+             + custom->getEmail() + "</a>");
+    setLabel(ui->lbAddress, custom->getAddress());
+    setLabel(ui->lbPostalCodeCityCountry,custom->getPostalCode()
+             + " " + custom->getCity());
+    setLabel(ui->lbFax, custom->getFax(), ui->lbTitleFax);
+
+}
+
+void CustomerDataWidget::setLabel(QLabel* lbValue, QString s, QLabel* lbLabel)
+{
+    if(s != "") {
+        lbValue->setText(s);
+        lbValue->show();
+        if(lbLabel != 0) {
+            lbLabel->show();
+        }
+    } else {
+        if(lbLabel != 0) {
+            lbLabel->hide();
+        }
+        lbValue->hide();
+    }
 }
