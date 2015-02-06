db990459d76d5d65cd2274bf269e1a27bb55449e
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-06 11:08:44 +0100

    Add coverage configuration

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
+. ./coverage.cfg
 
 lcov --capture --directory ../src --output-file coverage.info
-lcov -r coverage.info /opt/Qt/\* /usr/include/\* ~/projets/qt/FactDev/src/gui/\* ~/projets/qt/build-FactDev-Desktop_Qt_5_3_GCC_64bit-Debug/src/\* -o coverage.info 
-echo `date` >> tests.log
-genhtml coverage.info -o ../../FactDev/fact-team.github.io/out | tail -n3 > ../../FactDev/fact-team.github.io/coverage/logs/`date +"%Y-%m-%d"`.log
-echo '' >> tests.log
-echo '' >> tests.log
+lcov -r coverage.info $qtFolder/\* /usr/include/\* $projectFolder/src/gui/\* $buildFolder/src/\* -o coverage.info 
+genhtml coverage.info -o $projectFolder/fact-team.github.io/coverage | tail -n3 > $projectFolder/FactDev/fact-team.github.io/coverage/logs/`date +"%Y-%m-%d"`.log
