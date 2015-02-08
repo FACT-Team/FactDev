994508cf03442aba6df0c9c680d8ae29393aaa5e
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-08 15:37:04 +0100

    Add double quotes in coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -2,5 +2,6 @@
 # -*- coding: UTF8 -*-
 . ./coverage.cfg
 lcov --capture --directory ../src --output-file coverage.info
-lcov -r coverage.info $qtFolder/\* /usr/include/\* $projectFolder/src/gui/\* $buildFolder/src/\* -o coverage.info 
-genhtml coverage.info -o $projectFolder/fact-team.github.io/coverage | tail -n3 > $projectFolder/fact-team.github.io/coverage/logs/`date +"%Y-%m-%d"`.log
+lcov -r coverage.info "$qtFolder/\*" "/usr/include/\*" "$projectFolder/src/gui/\*" "$buildFolder/src/\*" -o coverage.info 
+genhtml coverage.info -o "$projectFolder/fact-team.github.io/coverage" | tail -n3 > "$projectFolder/fact-team.github.io/coverage/logs/`date
++"%Y-%m-%d"`.log"
