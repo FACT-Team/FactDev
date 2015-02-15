7197640a04d9e65f69aafb32a6c9eb4d557efbd0
Florent Berbie <florent-berbie@hotmail.fr>
2015-02-15 15:48:42 +0100

    Add tests

--- src/sql/tests/removeuselessdata.sql
+++ src/sql/tests/removeuselessdata.sql
@@ -3,17 +3,4 @@ where idCustomer in
 (SELECT idCustomer FROM Customer
 WHERE idCustomer not in (SELECT idCustomer FROM Project));
 
-delete from billing
-where idBilling IN
-(select idBilling from billing where idBilling
-not in (select idBilling from billingProject));
 
-delete from contributory
-where idCOntributory in
-(select idContributory from contributory where idContributory
-not in (select idContributory from billingProject));
-
-delete from project
-where idProject in
-(select idProject from project where idProject
-NOT in (select idProject from billingproject));
--- tests/models/searchtest.cpp
+++ tests/models/searchtest.cpp
@@ -37,8 +37,8 @@ void searchTest::searchCompanyNameWithSimpleQuote()
     _search.setSearchInProjects(false);
     _search.setSearchInContributories(false);
     _search.setSearchInBillsQuotes(false);
-    _search.setText("facil'isis'");
-    QVERIFY(_search.getFilter() == "AND (0 OR company LIKE '%facil''isis''%' )");
+    _search.setText("at'institute'");
+    QVERIFY(_search.getFilter() ==  "AND (0  OR company LIKE '%at''institute''%' )");
 
     QStandardItemModel* model = Databases::CustomerDatabase::instance()->getCustomersTable(_search.getFilter());
     QVERIFY(model->data(model->index(0, 2)) == QVariant::Invalid);
@@ -52,11 +52,65 @@ void searchTest::searchReferentLastname()
     _search.setSearchInProjects(false);
     _search.setSearchInContributories(false);
     _search.setSearchInBillsQuotes(false);
-    _search.setText("middleton");
-    QVERIFY(_search.getFilter() == "AND (0 OR lastnameReferent LIKE '%middleton%')");
+    _search.setText("haley");
+    QVERIFY(_search.getFilter() == "AND (0  OR lastnameReferent LIKE '%haley%' )");
     QStandardItemModel* model = Databases::CustomerDatabase::instance()->getCustomersTable(_search.getFilter());
-    QVERIFY(model->data(model->index(0, 2)).toString().toUpper() == "MIDDLETON");
+    QVERIFY(model->data(model->index(0, 2)).toString().toUpper() == "HALEY");
 
+}
+
+void searchTest::searchProjectName()
+{
+    _search.setGroupFilter(true);
+    _search.setSearchInCompanies(false);
+    _search.setSearchInReferentLastname(false);
+    _search.setSearchInProjects(true);
+    _search.setSearchInContributories(false);
+    _search.setSearchInBillsQuotes(false);
+    _search.setText("aliquam");
+    QVERIFY(_search.getFilter() == "AND (0 OR p.name LIKE '%aliquam%'  AND bp.idProject = p.idProject )");
+    QStandardItemModel* model = Databases::CustomerDatabase::instance()->getCustomersTable(_search.getFilter());
+    QVERIFY(model->data(model->index(0, 2)).toString().toUpper() == "DELGADO");
+
+}
+
+void searchTest::searchContributoryDescription()
+{
+    _search.setSearchInCompanies(false);
+    _search.setSearchInReferentLastname(false);
+    _search.setSearchInProjects(false);
+    _search.setSearchInContributories(true);
+    _search.setSearchInBillsQuotes(false);
+    _search.setText("amet, conse");
+    QVERIFY(_search.getFilter() == "AND (0  OR company LIKE '%amet, conse%'  OR bp.idContributory = ( SELECT idContributory FROM Contributory WHERE 0 OR description LIKE '%amet,%conse%' ))");
+    QStandardItemModel* model = Databases::CustomerDatabase::instance()->getCustomersTable(_search.getFilter());
+    QVERIFY(model->data(model->index(0, 2)).toString().toUpper() == "BELL");
+}
+
+void searchTest::searchBillOrQuoteTitle()
+{
+    _search.setSearchInCompanies(false);
+    _search.setSearchInReferentLastname(false);
+    _search.setSearchInProjects(false);
+    _search.setSearchInContributories(false);
+    _search.setSearchInBillsQuotes(true);
+    _search.setText("mollis nec");
+    QVERIFY(_search.getFilter() == "AND (0  AND 1 OR bp.idBilling = ( SELECT idBilling FROM Billing WHERE 0 OR title LIKE '%mollis%nec%'  OR 0 ))");
+    QStandardItemModel* model = Databases::CustomerDatabase::instance()->getCustomersTable(_search.getFilter());
+    QVERIFY(model->data(model->index(0, 2)).toString().toUpper() == "COTE");
+}
+
+void searchTest::searchBillOrQuoteNumber()
+{
+    _search.setSearchInCompanies(false);
+    _search.setSearchInReferentLastname(false);
+    _search.setSearchInProjects(false);
+    _search.setSearchInContributories(false);
+    _search.setSearchInBillsQuotes(true);
+    _search.setText("");
+    QVERIFY(_search.getFilter() == "");
+    QStandardItemModel* model = Databases::CustomerDatabase::instance()->getCustomersTable(_search.getFilter());
+    QVERIFY(model->data(model->index(0, 2)).toString().toUpper() == "");
 }
 
 void searchTest::searchWithoutFilters()
