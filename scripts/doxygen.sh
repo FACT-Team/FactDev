ad8b65cf82304d7b9502ed9227c2e09ccc1f1abd
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-02-26 13:28:57 +0100

    Remove generates files for doxygen

--- scripts/doxygen.sh
+++ scripts/doxygen.sh
@@ -2,6 +2,7 @@ cd ../fact-team.github.io/
 echo "#### GENERATE DOXYGEN ####"
 git checkout master
 cd ..
+make clean && rm ui_*
 doxygen >> /dev/null 2> fact-team.github.io/doc/doxygen.log
 cd fact-team.github.io/doc/latex
 echo "#### COMPILATION OF LATEX DOXYGEN####"
