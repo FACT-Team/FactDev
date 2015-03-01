6b72b08c744d115b5684d623596efed2b09a7bd8
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-01 01:04:52 +0100

    Continuation of windows powershell

--- deploy/windows.ps1
+++ deploy/windows.ps1
@@ -1,40 +1,83 @@
 #### CONFIGURATION ####
+## Warning: you must not have space in paths
 $qt_path = "C:\Qt\5.4\mingw491_32"
 $mingw_path = "C:\Qt\Tools\mingw491_32"
-$build_path = "C:\Documents and Settings\aroquemaurel\Documents"
-$repo_path = "C:\Documents and Settings\aroquemaurel\Documents\FactDev"
+$build_path = "C:\Users\Aroquemaurel\Documents\build-FactDev-Desktop_Qt_5_4_1_MinGW_32bit-Release"
+$repo_path = "C:\Users\Aroquemaurel\Documents\FactDev"
 $nb_process = 3
 
 
 
-$make_path = $mingw_path + "\bin\mingw32-make.exe "
+function call
+{
+    iex $args[0]  
+}
+
+function mkdirIfNotExist
+{
+    $dir = $args[0]
+    $ret = Test-Path $dir 
+    if (-not $ret) {
+       mkdir $dir
+    } 
+}
+
+function rmdirIfExist
+{
+    $dir = $args[0]
+    $ret = Test-Path $dir 
+    if ($ret) {
+       rmdir -r $dir
+    } 
+}
+
+function rmIfExist
+{
+    $file = $args[0]
+    $ret = Test-Path $file 
+    if ($ret) {
+       rm $file
+    } 
+}
+
+function copyPlugin 
+{
+    $plugin_name = $args[0]
+    mkdirIfNotExist $plugin_name
+    call ("cp "+$qt_path+"\plugins\"+$plugin_name+"\*.dll "+$plugin_name)
+    #call ("rm """+$plugin_name+"/*d.dll""")
+}
+
+function copyQtDll 
+{
+    $dll_name = $args[0]
+    call ("cp "+$qt_path+"\bin\"+$dll_name+".dll .")
+}
+
+
+
+$make_path = $mingw_path + "\bin\mingw32-make.exe"
 $qmake_path = $qt_path + "\bin\qmake.exe"
-$make_nbprocess = "-j"+$nb_process
+$make_nbprocess = " -j"+$nb_process
 
 cd $build_path
 
-
 #### COMPILATION ####
+## WARNING: We assume that FactDev is already compilled and is work correctly.
 # Create build folder
-mkdirIfNotExist build
-cd build
+#mkdirIfNotExist build
+#cd build
 
 # QMake execution
-call  ($qmake_path + """" +$repo_path + "\FactDev.pro"" -r -spec win32-g++")
+#call  ($qmake_path + " """ +$repo_path + "\FactDev.pro"" -config release -r -spec win32-g++")
 
 # Make clean, just in case
-call ($make_path+" clean")
+#call ($make_path + " clean")
 
-# Make of FactDev.dll
-cd src
-call ($make_path+ $make_nbprocess)
 
-cp release/FactDev.dll . #Move FactDev.dll in write location
-cd ../app
-call ($make_path+ $make_nbprocess)
 
 #### Copy dll files ####
-cd release
+cd app/release
 # Copy plugins folders
 copyPlugin "imageformats"
 copyPlugin "platforms"
@@ -54,51 +97,12 @@ copyQtDll "Qt5PrintSupport"
 copyQtDll "Qt5Sql"
 copyQtDll "Qt5Widgets"
 
-call ("cp ../FactDev.dll .") # Copy FactDev.dll
+rmdirIfExist sql
+call ("cp "+$build_path+"\src\release\FactDev.dll .") # Copy FactDev.dll
+call ("cp "+$repo_path+"\src\sql\* sql") # Copy sql files
+call ("cp "+$repo_path+"\fact-team.github.io\doc\usermanual.pdf manuel.pdf") # Copy sql files
+cp app.exe FactDev.exe
 
 
-
-
-#C:\Qt\5.4\mingw491_32\plugins
-#18:43:51: Débute : "C:\Qt\5.4\mingw491_32\bin\qmake.exe" C:\Users\Aroquemaurel\Documents\FactDev\FactDev.pro -r -spec win32-g++
-# cd vers src
-# make -j3
-# copie de la lib factdev vers app/../src
-# cd vers app
-# make -j3
-# Copie des fichiers sql vers app
-# Copie des dll vers app
-# Copie de FactDev.dll vers app
-# Copie des Modules Qt vers app
-# Rename app to FactDev
 # Execution de machintruc pour faire un installeur
-# pause
 
-
-function call
-{
-    iex $args[0]  
-}
-
-function mkdirIfNotExist
-{
-    $dir = $args[0]
-    $ret = Test-Path $dir 
-    if (-not $ret) {
-       mkdir $dir
-    } 
-}
-
-function copyPlugin 
-{
-    $plugin_name = $args[0]
-    mkdirIfNotExist $plugin_name
-    call ("cp "+$qt_path+"\plugins\"+$plugin_name+"\*.dll "+$plugin_name)
-    call ("rm """+$plugin_name+"/*d.dll""")
-}
-
-function copyQtDll 
-{
-    $dll_name = $args[0]
-    call ("cp "+$qt_path+"\bin\"+$dll_name+".dll .")
-}
