5e3e291699dd649e000a3805e09987383bf7e835
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-02 09:27:22 +0100

    Add tarball in linux deployment

--- deploy/linux.sh
+++ deploy/linux.sh
@@ -8,6 +8,8 @@ build_path="/home/aroquemaurel/projets/qt/build-factdev-release"
 repo_path="/home/aroquemaurel/projets/qt/FactDev"
 nb_process=3
 
+
+#### UTILS FUNCTIONS ####
 function copy_Qt_lib {
 	eval "cp -v $qt_path/lib/$1.* $build_path/app/"
 }
@@ -26,23 +28,32 @@ function mkdir_if_not_exists {
 mkdir_if_not_exists $build_path
 cd "$build_path"
 
+#### COMPILATION ####
+echo "#### COMPILATION ####"
+
 # QMake execution
+echo "--- QMake execution ---"
 eval "$qt_path/bin/qmake $repo_path/FactDev.pro -config release -r -spec linux-g++"
 
-#eval "$make_path clean" #Just in case…
+echo "--- Make clean ---"
+eval "$make_path clean" #Just in case…
 
 # Make 
+echo "--- Make ---"
 eval "$make_path -j$nb_process"
 
 #### Copy files ####
- 
+echo "### Copy files and lib"
+
 # Internal files
+echo "--- Copy FactDev library ---"
 eval "cp $build_path/src/*.so.* $build_path/app"
 eval "cp $repo_path/deploy/FactDev.sh $build_path/app"
 eval "chmod +x $build_path/app/FactDev.sh"
 eval "cp -r $repo_path/src/sql $build_path/app"
 
 # Qt library
+echo "--- copy Qt libraries ---"
 copy_Qt_lib libicuuc
 copy_Qt_lib libicui18n
 copy_Qt_lib libicudata
@@ -60,3 +71,9 @@ copy_Qt_plugin printsupport
 
 eval "rm $build_path/app/*prl"
 eval "rm $build_path/app/*la"
+
+#### CREATE TARBALL ####
+echo "--- Create FactDev tarball FactDev.tar.gz ---"
+eval "cd $build_path/"
+mv app FactDev
+tar -czvf FactDev.tar.gz FactDev 
