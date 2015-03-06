ea3ae3653b7d9b8103d9c233f602fd2da955e67f
Florent Berbie <florent-berbie@hotmail.fr>
2015-03-06 16:19:05 +0100

    Redefine Checkfield to QLineEdit in UserDataDialog

--- src/gui/dialogs/startedwindowsdialog.cpp
+++ src/gui/dialogs/startedwindowsdialog.cpp
@@ -1,12 +1,18 @@
 #include "startedwindowsdialog.h"
 #include "ui_startedwindowsdialog.h"
 
+namespace Gui {
+namespace Dialogs {
+
+
 StartedWindowsDialog::StartedWindowsDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::StartedWindowsDialog)
 {
     ui->setupUi(this);
     ui->wdgStack->setCurrentIndex(0);
+    _user = new User(1);
+    checkFields();
 }
 
 StartedWindowsDialog::~StartedWindowsDialog()
@@ -27,4 +33,69 @@ void StartedWindowsDialog::nextToPage3()
 void StartedWindowsDialog::saveSettings()
 {
 
+}
+
+
+void StartedWindowsDialog::accept() {
+    _user->setFirstname(ui->leFirstname->text());
+    _user->setLastname(ui->leLastname->text());
+//    _user->setCompany(ui->leCompany->text());
+//    _user->setTitle(ui->leTitle->text());
+//    _user->setAddress(ui->leAddress->text());
+//    _user->setCity(ui->leCity->text());
+//    _user->setPostalCode(ui->lePostalCode->text());
+//    _user->setEmail(ui->leEmail->text());
+//    _user->setPhone(ui->lePhone->text());
+//    _user->setMobilePhone(ui->leMobile->text());
+//    _user->setNoSiret(ui->leNoSiret->text());
+
+//    if (ui->leWorkspaceName->text().isEmpty()) {
+//        _user->setWorkspaceName("FactDev");
+//    } else {
+//        _user->setWorkspaceName(ui->leWorkspaceName->text());
+//    }
+
+//    if (ui->leWorkspacePath->text().isEmpty()) {
+//        _user->setWorkspacePath(
+//            QDir::homePath() + "/" +
+//            QStandardPaths::displayName(QStandardPaths::DocumentsLocation));
+//    } else {
+//        _user->setWorkspacePath(ui->leWorkspacePath->text());
+//    }
+
+//    _user->commit();
+    QDialog::accept();
+}
+
+
+void StartedWindowsDialog::checkFields() {
+//    ui->lePostalCode->setCountry("FRANCE");
+//    ui->lePhone->setCountry("FRANCE");
+//    ui->leMobile->setCountry("FRANCE");
+    ui->btnValidate->setEnabled(
+        ui->leFirstname->isValid() && ui->leLastname->isValid()
+//        && ui->leCompany->isValid() && ui->leTitle->isValid()
+//        && ui->leAddress->isValid() && ui->leCity->isValid()
+//        && ui->lePostalCode->isValid() && ui->leEmail->isValid()
+//        && ((ui->lePhone->isValid() && ui->leMobile->isValid())
+//            || (ui->lePhone->text().isEmpty() && ui->leMobile->isValid())
+//            || (ui->lePhone->isValid() && ui->leMobile->text().isEmpty()) )
+//                && ui->leNoSiret->isValid()
+       );
+}
+
+void StartedWindowsDialog::browseWorkspacePath()
+{
+//    QDir path;
+//    QFileDialog fileWindow( this,
+//                            "Espace de travail",
+//                            ui->leWorkspaceName->text(),
+//                            "*");
+//    fileWindow.exec();
+//    path = fileWindow.directory();
+
+//    ui->leWorkspacePath->setText(path.absolutePath());
+}
+
+}
 }
--- src/gui/dialogs/startedwindowsdialog.h
+++ src/gui/dialogs/startedwindowsdialog.h
@@ -2,10 +2,16 @@
 #define STARTEDWINDOWSDIALOG_H
 
 #include <QDialog>
-
+#include "models/user.h"
 namespace Ui {
 class StartedWindowsDialog;
 }
+
+using namespace Models;
+
+namespace Gui {
+namespace Dialogs {
+
 
 class StartedWindowsDialog : public QDialog
 {
@@ -15,7 +21,31 @@ public:
     explicit StartedWindowsDialog(QWidget *parent = 0);
     ~StartedWindowsDialog();
 
-private slots:
+    /**
+     * @brief UserDataDialog::fillFields Fill line edits with the data of the
+     * user
+     */
+    void fillFields();
+
+    /**
+     * @brief accept Valid data inputed by user and add these data in Database
+     */
+    void accept();
+
+
+public slots:
+    /**
+     * @brief UserDataDialog::checkFields Check all fields of dialog
+     * components
+     */
+    void checkFields();
+
+    /**
+     * @brief UserDataDialog::browseWorkspacePath Open a new window to define
+     * the workspace path of the user
+     */
+    void browseWorkspacePath();
+
     /**
      * @brief StartedWindowsDialog::nextToPage2
      */
@@ -33,6 +63,9 @@ private slots:
 
 private:
     Ui::StartedWindowsDialog *ui;
+    User* _user;
 };
 
+}
+}
 #endif // STARTEDWINDOWSDIALOG_H
