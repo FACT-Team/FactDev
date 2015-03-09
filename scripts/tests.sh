8e2e994d5022ced326f38f559c9ad7c20c0c589e
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-09 17:10:17 +0100

    Make -j15 to -j9

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,7 +1,7 @@
 cd ..
 echo "#### COMPILATION ####"
 ~/Qt5.2.0/5.2.0/gcc_64/bin/qmake FactDev.pro -r -spec linux-g++ CONFIG+=debug >> /dev/null
-make -j15 >> /dev/null
+make -j9 >> /dev/null
 LD_LIBRARY_PATH=`pwd`/src
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 ln -s `pwd`/src/sql `pwd`/tests/sql
