54cc62a416951b8f43fbb1e7c6a3a9ee65fb539a
Cédric Rohaut <rohaut@icloud.com>
2015-03-10 13:38:59 +0100

    add People class

---
+++ src/models/people.cpp
@@ -0,0 +1,140 @@
+#include "models/people.h"
+
+namespace Models {
+
+People::People()
+{
+
+}
+
+QString People::getFirstname() const
+{
+    return _firstname;
+}
+
+void People::setFirstname(const QString &firstname)
+{
+    _firstname = firstname;
+}
+
+QString People::getLastname() const
+{
+    return _lastname;
+}
+
+void People::setLastname(const QString &lastname)
+{
+    _lastname = lastname;
+}
+
+QString People::getCompany() const
+{
+    return _company;
+}
+
+void People::setCompany(const QString &company)
+{
+    _company = company;
+}
+
+QString People::getAddress() const
+{
+    return _address;
+}
+
+void People::setAddress(const QString &address)
+{
+    _address = address;
+}
+
+QString People::getPostalCode() const
+{
+    return _postalCode;
+}
+
+void People::setPostalCode(const QString &postalCode)
+{
+    _postalCode = postalCode;
+}
+
+QString People::getCity() const
+{
+    return _city;
+}
+
+void People::setCity(const QString &city)
+{
+    _city = city;
+}
+
+QString People::getCountry() const
+{
+    return _country;
+}
+
+void People::setCountry(const QString &country)
+{
+    _country = country;
+}
+
+QString People::getEmail() const
+{
+    return _email;
+}
+
+void People::setEmail(const QString &email)
+{
+    _email = email;
+}
+
+QString People::getPhone() const
+{
+    return _phone;
+}
+
+void People::setPhone(const QString &phone)
+{
+    _phone = phone;
+}
+
+QString People::getMobilePhone() const
+{
+    return _mobilePhone;
+}
+
+void People::setMobilePhone(const QString &mobilePhone)
+{
+    _mobilePhone = mobilePhone;
+}
+
+QString People::getFax() const
+{
+    return _fax;
+}
+
+void People::setFax(const QString &fax)
+{
+    _fax = fax;
+}
+
+bool People::operator ==(const People &p)
+{
+    return  p.getFirstname() == getFirstname()
+            && p.getLastname() == getLastname()
+            && p.getCompany() == getCompany()
+            && p.getAddress() == getAddress()
+            && p.getPostalCode() == getPostalCode()
+            && p.getCity() == getCity()
+            && p.getCountry() == getCountry()
+            && p.getEmail() == getEmail()
+            && p.getPhone() == getPhone()
+            && p.getMobilePhone() == getMobilePhone()
+            && p.getFax() == getFax();
+}
+
+bool People::operator !=(const People &u)
+{
+    return !(*this == u);
+}
+}
+
---
+++ src/models/people.h
@@ -0,0 +1,173 @@
+#ifndef PEOPLE_H
+#define PEOPLE_H
+#include <QString>
+
+namespace Models {
+/**
+ * @brief The People class People
+ */
+class People
+{
+public:
+    /**
+     * @brief People::People Construct a People
+     */
+    People();
+
+    /**
+     * @brief People::getFirstname Return the People firstname
+     * @return People firstname
+     */
+    QString getFirstname() const;
+    /**
+     * @brief People::setFirstnament Modify the People <i>firstname</i>
+     * @param firstname New People firstname
+     */
+    void setFirstname(const QString &firstname);
+
+    /**
+     * @brief People::getLastname Return the People lastname
+     * @return People lastname
+     */
+    QString getLastname() const;
+    /**
+     * @brief People::setLastname Modify the People <i>lastname</i>
+     * @param lastname New People lastname
+     */
+    void setLastname(const QString &lastname);
+
+    /**
+     * @brief People::getCompany Return the People company
+     * @return New company name
+     */
+    QString getCompany() const;
+    /**
+     * @brief People::setCompany Modify the People <i>company</i> name
+     * @param company New People company name
+     */
+    void setCompany(const QString &company);
+
+    /**
+     * @brief People::getAddress Return the company addess (Number and name of
+     * street)
+     * @return Address company
+     */
+    QString getAddress() const;
+    /**
+     * @brief People::setAddress Modify the People company <i>address</i>
+     * @param address Company address (name and number of street)
+     */
+    void setAddress(const QString &address);
+
+    /**
+     * @brief People::getPostalCode Return the postal code
+     * @return postal code
+     */
+    QString getPostalCode() const;
+    /**
+     * @brief People::setPostalCode Modify the postal code <i>postalCode</i>
+     * @param postalCode New postal code
+     */
+    void setPostalCode(const QString &postalCode);
+
+    /**
+     * @brief People::getCity Return the city
+     * @return city
+     */
+    QString getCity() const;
+    /**
+     * @brief People::setCity Modify the <i>city</i>
+     * @param city Company city address
+     */
+    void setCity(const QString &city);
+
+    /**
+     * @brief People::getCountry Return the country of the People
+     * @return country of the People
+     */
+    QString getCountry() const;
+    /**
+     * @brief People::setCountry Replace the country of the People
+     * by <i>country</i>
+     * @param country New country of the People
+     */
+    void setCountry(const QString &country);
+
+    /**
+     * @brief People::getEmail Return the People professional <i>email</i>
+     * @return professional email
+     */
+    QString getEmail() const;
+    /**
+     * @brief People::setEmail Modify the People professional <i>email</i>
+     * @param email The People professional email
+     */
+    void setEmail(const QString &email);
+
+    /**
+     * @brief People::getPhone Return the number of the desktop phone
+     * @return number of the desktop phone
+     */
+    QString getPhone() const;
+    /**
+     * @brief People::setPhone Modify the number of the desktop <i>phone</i>
+     * @param phone Number of the desktop phone
+     */
+    void setPhone(const QString &phone);
+
+    /**
+     * @brief People::getMobilePhone Return the number of the professional
+     * mobile phone
+     * @return number of mobile phone
+     */
+    QString getMobilePhone() const;
+    /**
+     * @brief People::setMobilePhone Modify the number of the professional People
+     * mobile phone <i>mobilePhone</i>
+     * @param mobilePhone Number of the professional mobile phone
+     */
+    void setMobilePhone(const QString &mobilePhone);
+    /**
+     * @brief People::getFax Return the fax number
+     * @return fax number
+     */
+    QString getFax() const;
+    /**
+     * @brief People::setFax Replace the current fax number by <i>fax</i>
+     * @param fax new fax number
+     */
+    void setFax(const QString &fax);
+
+    /**
+     * @brief People::operator == Re-define the operator "==" to compare if
+     * the current People is the same to the other <b>People</b> <i>c</i>
+     * Return TRUE if both Peoples are the same, else FALSE
+     * @param c People to compare
+     * @return boolean
+     */
+    bool operator ==(const People &c);
+    /**
+     * @brief People::operator == Re-define the operator "!=" to compare if
+     * the current People is differnt to the other <b>People</b> <i>c</i>
+     * Return TRUE if both Peoples are different, else FALSE
+     * @param c People to compare
+     * @return boolean
+     */
+    bool operator !=(const People &c);
+
+private:
+    QString _firstname;     //!< People firstname
+    QString _lastname;      //!< People lastname
+    QString _company;       //!< Company name of the People
+    QString _address;       //!< Name and number of street
+    QString _postalCode;    //!< Postal code
+    QString _city;          //!< City
+    QString _country;       //!< Country
+    QString _email;         //!< Professionnal email
+    QString _phone;         //!< Number of desktop phone
+    QString _mobilePhone;   //!< Professionnal number of mobile phone
+    QString _fax;           //!< Fax number of the people
+};
+}
+
+#endif // PEOPLE_H
