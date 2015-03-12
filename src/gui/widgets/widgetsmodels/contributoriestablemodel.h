6ba6ec0325be171efd04e4e1925ae6ac4cc01eae
Florent Berbie <florent-berbie@hotmail.fr>
2015-03-12 14:31:26 +0100

    Add documentation to WidgetsModels/

--- src/gui/widgets/widgetsmodels/contributoriestablemodel.h
+++ src/gui/widgets/widgetsmodels/contributoriestablemodel.h
@@ -17,33 +17,36 @@ namespace WdgModels {
 
 /**
  * @author Antoine de Roquemaurel
- * @brief The ContributoriesTableModel class for a custom table for contributories widget
+ * @brief The ContributoriesTableModel class for a custom table for
+ *  contributories widget
  * @see Contributory
  */
 class ContributoriesTableModel : public QAbstractTableModel
 {
 public:
     /**
-     * @brief ContributoriesTableModel Construct a ContributoriesTableModel
+     * @brief ContributoriesTableModel::ContributoriesTableModel Construct a
+     * ContributoriesTableModel
      * @param parent Parent widget
      */
     ContributoriesTableModel(QObject* parent = 0);
     ~ContributoriesTableModel();
 
     /**
-     * @brief rowCount Number of contributories row
+     * @brief ContributoriesTableModel::rowCount Number of contributories row
      * @return The number of contributories
      */
     int rowCount(const QModelIndex &) const;
 
     /**
-     * @brief columnCount Number of column of a contributory
+     * @brief ContributoriesTableModel::columnCount Number of column of a
+     * contributory
      * @return  The number of column
      */
     int columnCount(const QModelIndex &) const;
 
     /**
-     * @brief data Obtains data of a specify cell
+     * @brief ContributoriesTableModel::data Obtains data of a specify cell
      * @param index The cell who we want data
      * @param role The role of set
      * @return The data of cell
@@ -51,58 +54,71 @@ public:
     QVariant data(const QModelIndex &index, int role) const;
 
     /**
-     * @brief headerData Obtains header title of table
+     * @brief ContributoriesTableModel::headerData Obtains header title of table
      * @param section The number of column
      * @param orientation The table orientation
      * @param role
      * @return The Title header of column
      */
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    QVariant headerData(
+            int section, Qt::Orientation orientation, int role) const;
 
     /**
-      * @brief setData Change data of a cell
+      * @brief ContributoriesTableModel::setData Change data of a cell
       * @param index The cell to change data
       * @param value The new value
       * @param role THe role of cell
       * @return True if we could edit
       */
-     bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole);
+     bool setData(const QModelIndex & index,
+                  const QVariant & value,
+                  int role = Qt::EditRole);
 
      /**
-     * @brief append Add a new line in table
+     * @brief ContributoriesTableModel::append Add a new line in table
      * @param contributory The new contributory
      */
     void append(const Contributory & contributory);
 
     /**
-     * @brief remove Remove a line
+     * @brief ContributoriesTableModel::remove Remove a line
      * @param i The number of line to remove
      */
     void remove(const int i);
 
     /**
-     * @brief flags Differents table flags
+     * @brief ContributoriesTableModel::flags Differents table flags
      * @param index The cell who we want to know flags
      * @return Flags
      */
     Qt::ItemFlags flags(const QModelIndex & index) const ;
 
     /**
-     * @brief getContributories Get all contributories of table
+     * @brief ContributoriesTableModel::getContributories Get all contributories
+     *  of table
      * @return The contributory list
      */
     QList<Contributory> getContributories();
 
     /**
-     * @brief count Number of contributories in table
+     * @brief ContributoriesTableModel::count Number of contributories in table
      * @return The number of contributories
      */
     int count();
 
+    /**
+     * @brief ContributoriesTableModel::getSumQuantity Return the sum of the
+     * hours of all contriburoies added
+     * @return sum quantity
+     */
     double getSumQuantity() const;
+
+    /**
+     * @brief ContributoriesTableModel::clear Remove all contributories
+     */
     void clear();
 private:
-    QList<Contributory> _contributories; //!< contributories list
+    QList<Contributory> _contributories;          //!< contributories list
     QList<Contributory> _contributoriesToRemoved; //!< Contributories to removed
 };
 }
