db13d7daf6b290d263acc00e10846a0ecf6a527d
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-29 19:46:03 +0200

    Add Doxygen for Double.H

--- src/utils/double.h
+++ src/utils/double.h
@@ -3,9 +3,19 @@
 #include <math.h>
 
 namespace Utils {
+
+/**
+ * @brief The Double class Utils functions for Double calculs
+ */
 class Double
 {
 public:
+    /**
+     * @brief round Roud a double value to d decimals
+     * @param n The number
+     * @param d The number of decimals who you want
+     * @return The rounded value
+     */
     static double round(double n, unsigned int d);
 };
 }
