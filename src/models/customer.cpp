cd3a7f028cb800df49e35d9f18df55cc7a02ced2
Florent Berbie <florent-berbie@hotmail.fr>
2015-03-29 15:33:25 +0200

    Add new icons

--- src/models/customer.cpp
+++ src/models/customer.cpp
@@ -44,7 +44,8 @@ void Customer::hydrat(int id)
     setPhone(               customer->getPhone());
     setMobilePhone(         customer->getMobilePhone());
     setFax(                 customer->getFax());
-
+    setExtensionImage(      customer->getExtensionImage());
+    setImage(               customer->getImage());
 }
 
 void Customer::remove()
